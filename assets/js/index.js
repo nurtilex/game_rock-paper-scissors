215e776db132c7803acd9b78d4dc23b2938c4f0b
nurtilex <nurtilex@gmail.com>
2020-09-07 10:52:29 +0600

    made random choice of computer on gamer click. added score table. going to make game look better

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,47 @@
+const choices = {
+    rock: {
+        element: document.querySelector('.rock'),
+        path: 'assets/img/rock.png',
+    },
+    paper: {
+        element: document.querySelector('.paper'),
+        path: 'assets/img/paper.png',
+    },
+    scissors: {
+        element: document.querySelector('.scissors'),
+        path: 'assets/img/scissors.png',
+    },
+    computer: {
+        element: document.querySelector('.computer-choice'),
+        defaultImg: 'assets/img/question.png',
+    },
+    get randomChoiceImgPath() {
+        const allGamerChoiceImages = [ this.rock.path, this.paper.path, this.scissors.path ];
+        const randomIndex = Math.floor(Math.random() * allGamerChoiceImages.length);
+        return allGamerChoiceImages[randomIndex];
+    },
+    get allGamerChoiceElements() {
+        return document.querySelectorAll('.gamer-choice');
+    },
+    computerChoiceToDefault: function() {
+        this.computer.element.style.backgroundImage = `url(${this.computer.defaultImgPath})`;
+    },
+    disableChoices: function(boolean = true) {
+        this.allGamerChoiceElements.forEach(i => i.disabled = boolean);
+    }
+};
 
-const rock = document.querySelector('.rock img');
-const computerChoice = document.querySelector('.computer-choice img');
-const fistUrl = rock.getAttribute('src');
-rock.onclick = evt => {
-    evt.preventDefault();
-    computerChoice.classList.add('question-rotating-animation-class');
+
+choices.allGamerChoiceElements.forEach(item => {
+    item.addEventListener('click', function(evt) {
+        evt.preventDefault();
+        //choices.allGamerChoiceElements.forEach(i => i.disabled = true)
+        choices.disableChoices();
+        choices.computer.element.classList.add('question-rotating-animation-class');
     setTimeout( function() {
-        computerChoice.setAttribute('src', rock.getAttribute('src'));
-    }, 1000 )
-    
-}
+        choices.disableChoices(false);
+        choices.computer.element.style.backgroundImage = `url(${choices.randomChoiceImgPath})`;
+        choices.computer.element.classList.remove('question-rotating-animation-class');
+    }, 1000 );
+    })
+})
