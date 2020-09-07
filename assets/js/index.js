d975809cf5e1dd6f66baf9f5f66080255f1ba401
nurtilex <nurtilex@gmail.com>
2020-09-07 13:23:26 +0600

    ready for production

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,47 +1,79 @@
-const choices = {
-    rock: {
-        element: document.querySelector('.rock'),
-        path: 'assets/img/rock.png',
-    },
-    paper: {
-        element: document.querySelector('.paper'),
-        path: 'assets/img/paper.png',
-    },
-    scissors: {
-        element: document.querySelector('.scissors'),
-        path: 'assets/img/scissors.png',
-    },
-    computer: {
-        element: document.querySelector('.computer-choice'),
-        defaultImg: 'assets/img/question.png',
-    },
-    get randomChoiceImgPath() {
-        const allGamerChoiceImages = [ this.rock.path, this.paper.path, this.scissors.path ];
-        const randomIndex = Math.floor(Math.random() * allGamerChoiceImages.length);
-        return allGamerChoiceImages[randomIndex];
-    },
-    get allGamerChoiceElements() {
-        return document.querySelectorAll('.gamer-choice');
-    },
-    computerChoiceToDefault: function() {
-        this.computer.element.style.backgroundImage = `url(${this.computer.defaultImgPath})`;
-    },
-    disableChoices: function(boolean = true) {
-        this.allGamerChoiceElements.forEach(i => i.disabled = boolean);
-    }
-};
+const game = {
+	rock: {
+		element: document.querySelector('.rock'),
+		path: 'assets/img/rock.png',
+		get dataAttr() {return this.element.dataset.value},
+	},
+	paper: {
+		element: document.querySelector('.paper'),
+		path: 'assets/img/paper.png',
+		get dataAttr() {return this.element.dataset.value},
+	},
+	scissors: {
+		element: document.querySelector('.scissors'),
+		path: 'assets/img/scissors.png',
+		get dataAttr() {return this.element.dataset.value},
+	},
+	computer: {
+		element: document.querySelector('.computer-choice'),
+		defaultImg: 'assets/img/question.png',
+		dataAttr: 'default',
+		rotate: function(boolean) {
+			if (boolean) {this.element.classList.add('rotate')}
+			else {this.element.classList.remove('rotate')}
+		},
+	},
+	score: {
+		computer: document.querySelector('.computer-score'),
+		draw: document.querySelector('.draw-score'),
+		gamer: document.querySelector('.gamer-score'),
+	},
+	restartButton: document.querySelector('.restart'),
+
+	random: function() {
+		const allGamerChoices = [ this.rock, this.paper, this.scissors ]
+		const randomIndex = Math.floor(Math.random() * allGamerChoices.length)
+		this.computer.element.style.backgroundImage = `url(${allGamerChoices[randomIndex].path})`
+		this.computer.element.dataset.value = allGamerChoices[randomIndex].dataAttr
+	},
+	get allGamerChoiceElements() {
+		return document.querySelectorAll('.gamer-choice')
+	},
+	reset: function() {
+		this.computer.element.style.backgroundImage = `url(${this.computer.defaultImg})`
+		this.score.computer.textContent = 0
+		this.score.draw.textContent = 0
+		this.score.gamer.textContent = 0
+	},
+	disableChoices: function(boolean = true) {
+		this.allGamerChoiceElements.forEach(i => i.disabled = boolean)
+	},
+	compare: function(comp, gamer) {
+		if (comp === gamer)  this.score.draw.textContent++ 
+		else if (comp === 'rock' && gamer === 'scissors') this.score.computer.textContent++
+		else if (comp === 'rock' && gamer === 'paper') this.score.gamer.textContent++
+
+		else if (comp === 'paper' && gamer === 'rock') this.score.computer.textContent++
+		else if (comp === 'paper' && gamer === 'scissors') this.score.gamer.textContent++
+
+		else if (comp === 'scissors' && gamer === 'paper') this.score.computer.textContent++
+		else if (comp === 'scissors' && gamer === 'rock') this.score.gamer.textContent++
+	}
+}
 
 
-choices.allGamerChoiceElements.forEach(item => {
-    item.addEventListener('click', function(evt) {
-        evt.preventDefault();
-        //choices.allGamerChoiceElements.forEach(i => i.disabled = true)
-        choices.disableChoices();
-        choices.computer.element.classList.add('question-rotating-animation-class');
-    setTimeout( function() {
-        choices.disableChoices(false);
-        choices.computer.element.style.backgroundImage = `url(${choices.randomChoiceImgPath})`;
-        choices.computer.element.classList.remove('question-rotating-animation-class');
-    }, 1000 );
-    })
+game.allGamerChoiceElements.forEach(item => {
+	item.addEventListener('click', function() {
+		game.disableChoices()
+		game.computer.rotate(true)
+
+		setTimeout( function() {
+			game.disableChoices(false)
+			game.random()
+			game.computer.rotate(false)
+			game.compare(game.computer.element.dataset.value, item.dataset.value)
+		}, 1000 )
+	})
 })
+
+game.restartButton.onclick = () => game.reset()
